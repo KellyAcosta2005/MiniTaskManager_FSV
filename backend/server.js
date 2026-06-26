d78e50bc0d400ff2d7e5e838a87bc10342ee117f
Carlos Manuel Vargas <cmv-66@hotmail.com>
2026-06-26 19:34:39 -0300

    feat: init config server

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import app from './src/app.js';
+import {env} from "./src/config/env.js";
+
+const server = app.listen(env.PORT, env.HOST, () => {
+    console.log(`🚀 Server running on http://${env.HOST}:${env.PORT}`);
+})
+
+server.on("error", (error) => {
+  console.error("❌ Error starting server:", error.message);
+  process.exit(1);
+});
---
+++ backend/src/config/env.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const env = {
+    PORT: process.env.PORT || 3050,
+    HOST: process.env.HOST || '127.0.0.1',
+    MONGOBD_URI: process.env.MONGOBD_URI || "mongodb://localhost:27017",
+    JWT_SECRET: process.env.JWT_SECRET || "some_secret_key",
+    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "24h",
+}
